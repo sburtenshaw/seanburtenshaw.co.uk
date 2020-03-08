91b6dfda9f2f8b75d319d7ee518003b4becee6a2
Sean Burtenshaw <sean@lifecake.com>
2020-03-08 21:48:51 +0000

    Updated port. Test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('express')();
 
-const port = 8000;
+const port = 500;
 
 app.get('/', (req, res) => {
 	res.send('<h1>Love you loads</h1><p>Pleaseeeee make teaaaaa <3 <3 <3</p>');
